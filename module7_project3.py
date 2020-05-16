ad1c1d85ec6a3192925705cc5d039caf05b30e5e
Abhyudya <42513946+Abhyudyabajpai@users.noreply.github.com>
2020-05-16 21:20:51 +0530

    Carly's Clippers

---
+++ module7_project3.py
@@ -0,0 +1,26 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+for p in prices:
+  total_price = total_price+p
+
+average_price = total_price/len(prices)
+print(average_price)
+
+new_prices =[np-5 for np in prices]
+print(new_prices)
+
+total_revenue = 0
+
+for i in range(len(hairstyles)):
+  total_revenue = prices[i]+last_week[i]
+print(total_revenue)
+average_daily_revenue = total_revenue/7
+print(average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if prices[i] < 30]
+print(cuts_under_30)
