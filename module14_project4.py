4932f377c485845c93e011c717b79060bd085e72
Abhyudya <42513946+Abhyudyabajpai@users.noreply.github.com>
2020-05-29 13:35:19 +0530

    Describe Exam Grade Distributions

---
+++ module14_project4.py
@@ -0,0 +1,27 @@
+On exam 1, the average and median scores were 80 and 80, respectively. The distribution is symmetric with a similar distribution of scores to the left and right of the center.
+
+The range is close to 35, with the lowest grade close to 55 and the largest grade close to 90 There is one student, who scored close to 55 who is considered an outlier.
+
+#####################
+#####################
+
+On exam 2, the average and median scores were 82 and 84 respectively. The distribution has a left skew, which agrees with our finding that the average of our dataset is smaller than the median.
+
+The range is close to 38 with the lowest grade close to 60 and the largest grade close to 98
+
+#####################
+#####################
+
+On exam 3, the average and median scores were 77 and 80 respectively. The distribution is bimodal, and both modes have a similar tail on both sides of their peak, indicating that each is symmetric.
+
+The range is close to 42 with the lowest grade close to 56 and the largest grade close to 98.
+
+
+#####################
+#####################
+
+
+On the final exam, the average and median scores were 80 and 80 respectively. The distribution is symmetric with a similar distribution of scores to the left and right of the center.
+
+The range is close to 30 with the lowest grade close to 68 and the largest grade close to 98 There is one student, who scored close to 98 that is considered an outlier.
+
